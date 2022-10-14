938ae4d5f90d9b8e1eb59e646d663c45f14b2af1
Michael Alvarez <michaelalvarezxd@gmail.com>
2022-10-14 17:48:04 -0300

    Add header-navbar initial components and sass base

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Logo</h2>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <h4> Navbar</h4>;
+};
+
+export default Navbar;
