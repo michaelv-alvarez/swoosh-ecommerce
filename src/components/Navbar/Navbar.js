a7dc400b959f61608c8cc57fe5798d53bd09be08
Michael Alvarez <michaelalvarezxd@gmail.com>
2022-10-28 00:15:56 -0300

    Add custom dropdown menu for product categories

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import CartWidgets from "../CartWidgets/CartWidgets";
 import logo_letters from "../../logo_letters_transparent.png";
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => {
+    setOpen(!open);
+  };
   return (
     <nav className="header__nav">
       <a href="/" className="nav__link">
@@ -12,9 +16,24 @@ const Navbar = () => {
         Shop
       </a>
       <img src={logo_letters} alt="Logo letters" className="nav__logo" />
-      <a href="/" className="nav__link">
-        Products
-      </a>
+
+      <div className="dropdown-menu">
+        <button className="nav__link" onClick={handleOpen}>
+          Products
+        </button>
+        <div
+          className={`dropdown-menu__categories 
+          ${open ? "active" : "inactive"}`}
+        >
+          <a href="/" className="nav__link .nav__link--cateogory">
+            Categoria 1
+          </a>
+          <a href="/" className="nav__link .nav__link--cateogory">
+            Categoria 2
+          </a>
+        </div>
+      </div>
+
       <a href="/" className="nav__link">
         About
       </a>
