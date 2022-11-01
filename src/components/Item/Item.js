bcb712e764db5193d451d98c892dbac2afdd25f9
Michael Alvarez <michaelalvarezxd@gmail.com>
2022-11-01 15:43:38 -0300

    Change image and font properties

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+const temp_img =
+  "https://res.cloudinary.com/dtpfo51rx/image/upload/v1667322354/swoosh/products/img/jackets/85-Vintage-Nike-Puffer-Jacket-1_large-removebg-preview_pmzzlz.png";
 const Item = ({ title, image, price, id, description, showAs }) => {
   if (showAs === "Detail") {
     return (
       <div className="detail__card card">
-        <img src={image} alt={title} className="card__image" />
+        <img src={temp_img} alt={title} className="card__image" />
         <div className="card__body">
           <h3 className="card__title">{title}</h3>
           <div className="card__description">{description}</div>
@@ -18,7 +19,7 @@ const Item = ({ title, image, price, id, description, showAs }) => {
     <>
       <Link to={`/item/detail/${id}`}>
         <div className="product__card card">
-          <img src={image} alt={title} className="card__image" />
+          <img src={temp_img} alt={title} className="card__image" />
           <div className="card__footer">
             <h3 className="card__title">{title}</h3>
             <span className="card__price">${price}</span>
